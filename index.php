486c76c4e9f3a570478e40b4cf809006ad9b94c5
aulitafanisa <aulitafanisa@gmail.com>
2026-04-23 02:01:11 +0700

    nambah href

--- index.php
+++ index.php
@@ -6,7 +6,7 @@
     <title>EyeCare - Jaga Kesehatan Mata</title>
     
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
-   <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap">
 
     <style> 
 
@@ -190,8 +190,8 @@
                     <li class="nav-item"><a class="nav-link px-3" href="#">Kontak</a></li>
                 </ul>
                 <div class="d-flex gap-3">
-                    <button class="btn btn-outline-light px-4 border-2 fw-bold">Masuk</button>
-                    <button class="btn btn-light px-4 fw-bold" style="color: #10367d;">Daftar</button>
+                     <a href="login.php" class="btn btn-outline-light px-4 border-2 fw-bold">Login</a>
+                    <a href="register.php" class="btn btn-light px-4 fw-bold" style="color: #10367d">Daftar</a>
                 </div>
             </div>
         </div>
